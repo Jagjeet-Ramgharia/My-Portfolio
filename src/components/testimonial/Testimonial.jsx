b21a7e698fd6a33a52ecf6b73979f74ff0dcc52a
jagjeet ramgharia <ramghariajagjeet4281@gmail.com>
2021-08-20 21:22:17 +0530

    updated

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -4,15 +4,6 @@ import PhoneAndroid from "@material-ui/icons/PhoneAndroid";
 const Testimonial = () => {
   const data = [
     {
-      id: 1,
-      name: "Jagjeet Ramgharia",
-      icon: <PhoneAndroid />,
-      title: "Junior Developer",
-      desc: "elit Numquam reiciendis quo beatae ipsum, architecto ullam.",
-      img: "assets/1.png",
-      featured: false,
-    },
-    {
       id: 2,
       icon: <PhoneAndroid />,
       name: "Jagjeet Ramgharia",
@@ -20,15 +11,6 @@ const Testimonial = () => {
       desc: "I am a web developer with enthusiasm and focused on  frontend developer. I am always willing to work outside my comfort zone and give my best. I have a passion for building front-end interfaces of supreme quality. I love javascript ecosystem.",
       img: "assets/1.png",
       featured: true,
-    },
-    {
-      id: 3,
-      icon: <PhoneAndroid />,
-      name: "Jagjeet Ramgharia",
-      title: "MERN Stack",
-      desc: "elit Numquam reiciendis quo beatae ipsum, architecto ullam.",
-      img: "assets/1.png",
-      featured: false,
     },
   ];
 
